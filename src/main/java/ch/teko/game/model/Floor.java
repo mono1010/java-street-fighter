780ac24dbc49445075cc8bc4d26e41f0bac5c0aa
mono <2659311-Mono606@users.noreply.gitlab.com>
2024-06-18 11:51:23 +0200

    docs: floor documentation

--- src/main/java/ch/teko/game/model/Floor.java
+++ src/main/java/ch/teko/game/model/Floor.java
@@ -3,28 +3,56 @@ package ch.teko.game.model;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * Represents the game Floor bounds and graphical rendering.
+ */
 public class Floor {
+    /**
+     * The position of the floor.
+     */
     private int height;
 
+    /**
+     * Instance of Floor for singleton pattern.
+     */
     private static Floor instance;
 
+    /**
+     * Constructor for Floor.
+     */
     public Floor() {
         
     }
 
+    /**
+     * Sets the position of the floor.
+     * @param height The position to set for the floor.
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    /**
+     * Gets the position of the floor.
+     * @return The current position of the floor.
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Renders the floor on a given graphics context.
+     * @param g The graphics context to render on.
+     */
     public void onRender(Graphics g) {
         g.setColor(Color.RED);
         g.drawLine(0, this.height, 100000, this.height);
     }
 
+    /**
+     * Provides access to the singleton instance of Floor.
+     * @return The single instance of Floor.
+     */
     public static Floor getInstance() {
         if (instance == null) {
             instance = new Floor();
