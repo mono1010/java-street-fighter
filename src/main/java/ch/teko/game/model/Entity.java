c31cde5a2d29e0890ad27eee9eb8bb6f89b7ccdf
mono <2659311-Mono606@users.noreply.gitlab.com>
2024-06-18 11:43:43 +0200

    feat: javaDocs implementation

--- src/main/java/ch/teko/game/model/Entity.java
+++ src/main/java/ch/teko/game/model/Entity.java
@@ -3,9 +3,39 @@ package ch.teko.game.model;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+/**
+ * The {@code Entity} class represents a generic entity with position and velocity.
+ * This class provides the basic framework for entity objects.
+ */
 public abstract class Entity {
-    protected int x, y, velocityX, velocityY;
+    /**
+     * The x-coordinate of the entity.
+     */
+    protected int x;
 
+    /**
+     * The y-coordinate of the entity.
+     */
+    protected int y;
+
+    /**
+     * The velocity of the entity along the x-axis.
+     */
+    protected int velocityX;
+
+    /**
+     * The velocity of the entity along the y-axis.
+     */
+    protected int velocityY;
+
+    /**
+     * Constructs a new {@code Entity} with specified position and velocity.
+     *
+     * @param x         the initial x-coordinate of the entity
+     * @param y         the initial y-coordinate of the entity
+     * @param velocityX the initial velocity of the entity along the x-axis
+     * @param velocityY the initial velocity of the entity along the y-axis
+     */
     public Entity(int x, int y, int velocityX, int velocityY) {
         this.x = x;
         this.y = y;
@@ -13,8 +43,22 @@ public abstract class Entity {
         this.velocityY = velocityY;
     }
 
+    /**
+     * Updates the state of the entity every game tick.
+     */
     public abstract void onTick();
+
+    /**
+     * Renders the entity on the given graphics context.
+     *
+     * @param g the graphics context to render on
+     */
     public abstract void onRender(Graphics g);
 
+    /**
+     * Returns an axis-aligned bounding box representing the physical bounds of the entity.
+     *
+     * @return a {@code Rectangle} object that encloses the entity's bounds
+     */
     public abstract Rectangle getAABB();
 }
