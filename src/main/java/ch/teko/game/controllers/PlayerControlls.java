c982474651dd97a73f391c3d4957c889bb9c87bb
mono <2659311-Mono606@users.noreply.gitlab.com>
2024-06-23 16:09:38 +0200

    docs: player controls

--- src/main/java/ch/teko/game/controllers/PlayerControlls.java
+++ src/main/java/ch/teko/game/controllers/PlayerControlls.java
@@ -1,10 +1,43 @@
 package ch.teko.game.controllers;
 
+/**
+ * Represents player controls for movement and attacks.
+ */
 public class PlayerControlls {
-    public boolean up, down, right, left;
-    public boolean attack1, attack2;
+    /**
+     * Indicates whether the player is moving up.
+     */
+    public boolean up;
 
+    /**
+     * Indicates whether the player is moving down.
+     */
+    public boolean down;
+
+    /**
+     * Indicates whether the player is moving right.
+     */
+    public boolean right;
+
+    /**
+     * Indicates whether the player is moving left.
+     */
+    public boolean left;
+
+    /**
+     * Indicates whether the player is performing attack 1.
+     */
+    public boolean attack1;
+
+    /**
+     * Indicates whether the player is performing attack 2.
+     */
+    public boolean attack2;
+
+    /**
+     * Constructs a new instance of the PlayerControls class.
+     */
     public PlayerControlls() {
-
+        
     }
 }
