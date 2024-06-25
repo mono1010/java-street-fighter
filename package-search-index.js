a1a9a40388ad106d67842fb75234f19260a55fc8
mono1010 <74413262+mono1010@users.noreply.github.com>
2024-06-25 07:01:23 +0000

    Deploying to javadoc from @ mono1010/java-street-fighter@921974ba1601d30b7acc4dda3235f2ef35b16f19 🚀

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.teko.game"},{"l":"ch.teko.game.controllers"},{"l":"ch.teko.game.model"},{"l":"ch.teko.game.view"}];updateSearchResults();
--- run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn compile
-mvn package
-java -jar target/street-fighter-0.1.0.jar $1
