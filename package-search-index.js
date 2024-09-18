56c6a8023da1e3224a086a24223afc386d6f5b60
mono1010 <74413262+mono1010@users.noreply.github.com>
2024-09-18 20:11:33 +0000

    Deploying to javadoc from @ mono1010/java-street-fighter@a8a685c9fd2ab57c9f253196a57b43477fb119e2 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.teko.game"},{"l":"ch.teko.game.controllers"},{"l":"ch.teko.game.model"},{"l":"ch.teko.game.view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ch.teko.game"},{"l":"ch.teko.game.controllers"},{"l":"ch.teko.game.model"},{"l":"ch.teko.game.utils"},{"l":"ch.teko.game.view"}];updateSearchResults();
